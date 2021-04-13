390bb1459a66af88e89efa0da5930b15413ec8c7
juatabot <hengjuat@gmail.com>
2021-04-13 18:10:41 -0400

    tidy tweets

--- twitter.py
+++ twitter.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import twint
 import pandas as pd
@@ -9,31 +10,49 @@ def ngrams(input, n):
         output.append(input[i:i+n])
     return output
 
-dates = []
-with open('realdonaldtrump.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(spamreader)
-    for row in spamreader:
-        date_str = row[3][:10].split("-")
-        date_time = datetime(int(date_str[0]), int(date_str[1]), int(date_str[2]))
-        dates.append(date_time)
-        dates = list(set(dates))
+def get_matching_tweets_by_date():
+    dates = []
+    with open('realdonaldtrump.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reader)
+        for row in reader:
+            date_str = row[3][:10].split("-")
+            date_time = datetime(int(date_str[0]), int(date_str[1]), int(date_str[2]))
+            dates.append(date_time)
+            dates = list(set(dates))
 
-dates.sort()
+    dates.sort()
 
-dates = dates[818:]
+    dates = dates[818:]
 
-day_ranges = ngrams(dates, 2)
+    day_ranges = ngrams(dates, 2)
 
-c = twint.Config()
-c.Search = "@realdonaldtrump"
-c.Lang = "en"
-c.Limit = 500
-c.Popular_tweets = True
+    c = twint.Config()
+    c.Search = "@realdonaldtrump"
+    c.Lang = "en"
+    c.Limit = 500
+    c.Popular_tweets = True
 
-for day in day_ranges:
-    c.Since = str(day[0])
-    c.Until = str(day[1])
-    c.Output = f"scraped_tweets/{str(day[0])}.csv"
-    twint.run.Search(c)
+    for day in day_ranges:
+        c.Since = str(day[0])
+        c.Until = str(day[1])
+        c.Output = f"scraped_tweets/{str(day[0])}.csv"
+        twint.run.Search(c)
 
+def clean_csvs(path):
+    ls = os.listdir(path)
+    for f in ls:
+        with open(f'{path}/{f}') as csvfile:
+            # reader = csv.reader(csvfile)
+            for row in csvfile:
+                split = row.split()
+                tweet = " ".join(split[5:])
+                tidy_tweet = tweet.strip().encode('ascii', 'ignore')
+                print(tidy_tweet)
+
+        break
+
+
+if __name__ == "__main__":
+    clean_csvs("scraped_tweets")
+    
